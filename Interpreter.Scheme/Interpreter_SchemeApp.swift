e541b493af6918337484ae54d0984c3713d6e0ba
Pter <longpp17@163.com>
2024-05-16 23:56:13 +0200

    Initial Commit

---
+++ Interpreter.Scheme/Interpreter_SchemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interpreter_SchemeApp.swift
+//  Interpreter.Scheme
+//
+//  Created by apple on 2024/5/16.
+//
+
+import SwiftUI
+
+@main
+struct Interpreter_SchemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Interpreter.SchemeUITests/Interpreter_SchemeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Interpreter_SchemeUITestsLaunchTests.swift
+//  Interpreter.SchemeUITests
+//
+//  Created by apple on 2024/5/16.
+//
+
+import XCTest
+
+final class Interpreter_SchemeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
